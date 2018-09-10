17ddc69ce7131d365b4c9b61558ad73dfb67e59d
John Walczyk <jwalczyk@us.ibm.com>
2018-09-10 15:42:32 -0400

    first push

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+import json
+import time
+class HandleWebHookEvents():
+     def test(self):
+         msg = "Invalid toolchainId: asdf-dfgh-wert-erty"
+         rc = {"response": msg}
+         return None, (rc, 400)
+
+a=HandleWebHookEvents()
+
+t,tt,ttt = a.test()
+
+print t
+print tt
+print ttt
+print ttt
+print ttt
+print ttt
+print ttt
+print ttt
+print ttt
+print ttt
